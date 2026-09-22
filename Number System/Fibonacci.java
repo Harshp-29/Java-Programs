2955684cd433a847034c7c7948eb4636f596c3ff
Harshwardhan Patil <harshwardhan2929@gmail.com>
2026-09-22 16:19:52 +0530

    Updated

--- Number System/Fibonacci.java
+++ Number System/Fibonacci.java
@@ -3,8 +3,18 @@ class Fibonacci
 {
     public static void main(String []args)
     {
-        Scanner sc=new Scanner(Systtem.in);
+        Scanner sc=new Scanner(System.in);
         System.out.println("Enter the number: ");
         int num=sc.nextInt();
+        int a=0;
+        int b=1;
+        int c;
+
+        for(int i=1; i<=num ; i++){
+            System.out.print(a + " ");
+            c=a+b;
+            a=b;
+            b=c;
+        }
     }
 }
